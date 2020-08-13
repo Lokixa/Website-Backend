113a95a8ff6f298ca6f4b20653f06ef160e4ca92
Lokixa <lyuben.kodinov@gmail.com>
2020-08-13 12:55:09 +0300

    Sanitization and cors unique origin

--- app.js
+++ app.js
@@ -1,25 +1,35 @@
 var express = require('express')
 var path = require('path')
-var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 var cors = require('cors')
+var helmet = require('helmet')
+var expressSanitizer = require('express-sanitizer')
 
 var projectsRouter = require('./routes/projects')
 var mongoSetup = require('./mongo/mongo-setup')
 
 var app = express()
+const allowedOrigin = 'http://192.168.0.101:3000'
 
 app.set('trust proxy', true)
 app.set('trust proxy', 'loopback')
 
-app.use(cors())
+app.use(cors({ origin: allowedOrigin }))
+app.use(helmet())
+app.use(expressSanitizer())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 mongoSetup()
 app.use('/projects', projectsRouter)
+app.use((err, req, res, next) => {
+	if (err) {
+		console.error(err)
+		next(err)
+	}
+	next()
+})
 
 module.exports = app
--- routes/projects.js
+++ routes/projects.js
@@ -3,34 +3,33 @@ var router = express.Router()
 const db = require('../mongo/mongo-db')
 const { body, param, validationResult } = require('express-validator')
 
-const checks = [
-	body('name').isLength({ min: 3, max: 30 }),
-	body('url').isURL().isLength({ min: 12, max: 40 }),
-	body('language').isLength({ min: 1, max: 10 }),
-	body('description').isLength({ min: 3, max: 150 }),
-	body('license').isLength({ min: 1, max: 10 }),
-]
-
+const bodyChecks = {
+	name: body('name').isLength({ min: 3, max: 30 }),
+	url: body('url').isURL().isLength({ min: 12, max: 100 }),
+	language: body('language').isLength({ min: 1, max: 10 }),
+	description: body('description').isLength({ min: 3, max: 150 }),
+	license: body('license').isLength({ min: 1, max: 10 }),
+}
 router.get('/', async function (req, res) {
 	const docs = await db.find({})
 	res.json(docs)
 })
-router.get('/:name', async (req, res) => {
-	const doc = await db.find({ name: req.params.name })
-	res.json(doc)
-})
+router.get(
+	'/:name',
+	param('name').isLength({ min: 3, max: 20 }),
+	async (req, res) => {
+		const doc = await db.find({ name: req.sanitize(req.params.name) })
+		res.json(doc)
+	}
+)
 router.post(
 	'/',
 	[
-		body('name').notEmpty().isLength({ min: 3, max: 30 }),
-		body('url').notEmpty().isURL().isLength({ min: 12, max: 100 }),
-		body('language').notEmpty().isLength({ min: 1, max: 10 }),
-		body('description')
-			.if((value, { req }) => req.body.description)
-			.isLength({ min: 3, max: 150 }),
-		body('license')
-			.if((value, { req }) => req.body.license)
-			.isLength({ min: 1, max: 10 }),
+		bodyChecks.name.notEmpty(),
+		bodyChecks.url.notEmpty(),
+		bodyChecks.language.notEmpty(),
+		bodyChecks.description.if((value, { req }) => req.body.description),
+		bodyChecks.license.if((value, { req }) => req.body.license),
 	],
 	async (req, res) => {
 		const errors = validationResult(req)
@@ -45,22 +44,12 @@ router.post(
 router.put(
 	'/:name',
 	[
-		body('name')
-			.if((value, { req }) => req.body.name)
-			.isLength({ min: 3, max: 30 }),
-		body('url')
-			.if((value, { req }) => req.body.url)
-			.isURL()
-			.isLength({ min: 12, max: 40 }),
-		body('language')
-			.if((value, { req }) => req.body.language)
-			.isLength({ min: 1, max: 10 }),
-		body('description')
-			.if((value, { req }) => req.body.description)
-			.isLength({ min: 3, max: 150 }),
-		body('license')
-			.if((value, { req }) => req.body.license)
-			.isLength({ min: 1, max: 10 }),
+		param('name').isLength({ min: 3, max: 20 }).isAscii(),
+		bodyChecks.name.if((value, { req }) => req.body.name),
+		bodyChecks.url.if((value, { req }) => req.body.url),
+		bodyChecks.language.if((value, { req }) => req.body.language),
+		bodyChecks.description.if((value, { req }) => req.body.description),
+		bodyChecks.license.if((value, { req }) => req.body.license),
 	],
 	async (req, res) => {
 		const errors = validationResult(req)
@@ -69,20 +58,23 @@ router.put(
 			return
 		}
 
-		const doc = await db.updateOne({ name: req.params.name }, req.body)
+		const doc = await db.updateOne(
+			{ name: req.sanitize(req.params.name) },
+			req.body
+		)
 		res.json(doc)
 	}
 )
 router.delete(
 	'/:name',
-	[param('name').notEmpty().isLength({ min: 3, max: 30 })],
+	[param('name').isLength({ min: 3, max: 30 }).isAscii()],
 	async (req, res) => {
 		const errors = validationResult(req)
 		if (!errors.isEmpty()) {
 			res.status(400).json({ errors: errors.array() })
 			return
 		}
-		const doc = await db.deleteOne({ name: req.params.name })
+		const doc = await db.deleteOne({ name: req.sanitize(req.params.name) })
 		res.json(doc)
 	}
 )
